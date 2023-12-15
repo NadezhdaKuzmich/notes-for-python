186afdc339ffaa2211bb95503c65f2b8824430f1
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2023-12-15 02:15:30 +0100

    add dictionary

---
+++ dictionaries/dict.py
@@ -0,0 +1,74 @@
+# Словники – це впорядковані колекції формату ключ:значення. Значенням може бути будь-який об'єкт,
+# ключем може бути лише незмінний об'єкт.
+
+# Синтаксис словників: { ‘key’ : ‘value’ }
+# Де key – це ключ, яким доступний елемент value. Для того щоб отримати наше значення по ключу,
+# достатньо після назви словника в квадратних дужках вказати назву ключа. Також можна використовувати
+# метод get(), який у разі відсутності елемента за таким ключем поверне None.
+
+example_dict = {
+    "key": "value"
+}
+
+# Створення списків:
+# v1 { }
+my_dict = {
+    'name': 'Nadia',
+    'age': 27
+}
+print('v1:', my_dict)
+
+# v2 (список списків)
+my_dict = dict([['name', 'Nadia'], ['age', 27]])
+print('v2:', my_dict)
+
+# v3 (список (key, value))
+my_dict = dict([('name', 'Nadia'), ('age', 27)])
+print('v3:', my_dict)
+
+# v4 (за допомогою аргументів)
+my_dict = dict(name="Nadia", age=27)  # Але таким чином не можна додати ключ з типом int (числа)
+# my_dict2 = dict(18="hi", 32='by') # SyntaxError: expression cannot contain assignment, perhaps you meant "=="?
+print('v4:', my_dict)
+
+# get value from dict
+print(my_dict["name"])  # => Nadia
+# print(example_dict["key_no_exist"])  # => KeyError: 'key_no_exist'
+
+print(my_dict.get("age"))  # => 27
+# print(example_dict.get("key_no_exist"))  # => None
+
+# empty dictionary
+# empty_dict = {}
+city_dict = dict()
+
+# put values in dict
+city_dict['Kharkiv'] = 1654
+city_dict['Chuhuiv'] = 1638
+print(city_dict)
+
+# dictionaries support
+city_dict['Kharkiv'] = {'Year of foundation': 1654, 'Main rivers': ['Kharkiv', 'Lopan']}
+print(city_dict)
+print('~' * 40)
+
+# for in dict
+# v1
+for city in city_dict:
+    print('v1', city, city_dict[city])
+print('~' * 40)
+
+# v2
+for city in city_dict:
+    print(f'v2: {city}: {city_dict[city]}')
+print('~' * 40)
+
+# v3
+for key, value in city_dict.items():
+    # print(f'v3: {key}: {value}')
+    print(f'v3: {key=} {value=}')  # => ... key='Chuhuiv' value=1638
+print('~' * 40)
+
+# del
+del city_dict['Chuhuiv']
+print(city_dict)
---
+++ dictionaries/dictMethods.py
@@ -0,0 +1,135 @@
+# Методи словників:
+# help({})
+# help(dict)
+# dir(dict)
+
+capital_cities = {
+    'Venezuela': 'Caracas',
+    'Nicaragua': 'Managua',
+    'Ukraine': 'Kiev',
+    'Georgia': 'Tbilisi',
+    'Moldova': 'Kishinev',
+    'China': 'Beijing',
+    'USA': 'Washington',
+}
+
+# items() – повертає пару ключ – значення.
+print(capital_cities.items())
+# dict_items([('Venezuela', 'Caracas'), ('Nicaragua', 'Managua'), ('Ukraine', 'Kiev'), ('Georgia', 'Tbilisi'),
+# ('Moldova', 'Kishinev'), ('China', 'Beijing'), ('USA', 'Washington')])
+for element in capital_cities.items():
+    print(element)  # => ('Venezuela', 'Caracas')...
+print('~' * 40)
+
+# Розпаковка:
+# first_value, second_value = 1, 2
+# country, capital = 'Georgia', 'Tbilisi'
+
+for country, capital in capital_cities.items():
+    print(f'{country}: {capital}')  # => Venezuela: Caracas...
+print('~' * 40)
+
+# pop(key) – видаляє ключ і повертає відповідне значення.
+print(capital_cities.pop('China'))  # => Beijing
+print(capital_cities)
+print('~' * 40)
+
+# popitem() – видаляє останнє значення та повертає його у форматі ключ-значення. Не приймає аргументів.
+print(capital_cities.popitem())  # => ('USA', 'Washington')
+print(capital_cities)
+print('~' * 40)
+
+# update({‘key’: ‘value’}) – додає нове ключ-значення.
+capital_cities.update({'Brazil': 'Brasilia'})
+print(capital_cities)
+print('~' * 40)
+
+usa = {'USA': 'Washington'}
+capital_cities.update(usa)
+print(capital_cities)
+print('~' * 40)
+
+# keys() – повертає всі ключі у словнику.
+print(capital_cities.keys())
+# => dict_keys(['Venezuela', 'Nicaragua', 'Ukraine', 'Georgia', 'Moldova', 'Brazil', 'USA'])
+print('~' * 40)
+
+# values() – повертає всі значення у словнику.
+print(capital_cities.values())
+# => dict_values(['Caracas', 'Managua', 'Kiev', 'Tbilisi', 'Kishinev', 'Brasilia', 'Washington'])
+print('~' * 40)
+
+# setdefault() - Повертає значення, або як що ключа немає то повертає значення із аргументу.
+print('GET =>', capital_cities.get('Romania'))  # => None
+print('SETDEFAUL =>', capital_cities.setdefault('Romania', 'Bucharest'))  # => Bucharest
+print('~' * 40)
+
+# fromkeys - будує новий словник.
+any_dictionary = {}
+days_name_list = ['Monday', 'Tuesday', 'Wednesday']
+new_dictionary = any_dictionary.fromkeys(days_name_list)
+print(new_dictionary)  # => {'Monday': None, 'Tuesday': None, 'Wednesday': None}
+new_dictionary = any_dictionary.fromkeys(days_name_list, 'Day')
+print(f'{any_dictionary=}')  # => any_dictionary={}
+print(f'{new_dictionary=}')  # => new_dictionary={'Monday': 'Day', 'Tuesday': 'Day', 'Wednesday': 'Day'}
+print('~' * 40)
+
+# clear() – очищає словник.
+capital_cities.clear()
+print('clear =>', capital_cities)  # => {}
+print('~' * 40)
+
+# Задачки:
+# 1. Порахувати за допомогою словника скільки разів елемент повторюється у списку.
+classmates_name = ['Sergey', 'Igor', 'Tanya', 'Sergey', 'Mikhael', 'Sergey', 'Lena']
+
+# v1
+# answer = {}
+# for name in classmates_name:
+#     if name in answer.keys():
+#         answer[name] += 1
+#     else:
+#         answer[name] = 1
+# print(answer)
+# print('~' * 40)
+
+# v2
+answer = {}
+for name in classmates_name:
+    answer[name] = classmates_name.count(name)
+
+print(answer)
+print('~' * 40)
+
+# 2. Пройтися за словником і вивести всі значення, які мають парний ключ.
+data_dict = {1: 'one', 8: 'any text', 2: '2', 6: '6', 9: 'end'}
+for key, value in data_dict.items():
+    if key % 2 == 0:
+        print(value)
+print('~' * 40)
+
+# range_none: dict = dict().fromkeys(range(10), 'any value')
+# for k in range_none.keys():
+#     if k % 2 == 0:
+#         print(k)
+
+# 3. Видалити всі ключі, значення яких починається з літери.
+# v1
+# DO NOT delete element in the collection during cycle
+# only_int_keys: dict = {1: 'value', 'key': 123, 2: 'value', 'key2': 123}
+# for k in only_int_keys:
+#     if type(k) == str:
+#         del only_int_keys[k]
+#
+# print(only_int_keys)  # => RuntimeError: dictionary changed size during iteration
+
+# v 2
+
+# Видалити всі ключі, значення яких починається з літери.
+only_int_keys: dict = {1: 'value', 'key': 123, 2: 'value', 'key2': 123}
+copy_only_int_keys = only_int_keys.copy()
+for k in only_int_keys:
+    if type(k) is str:
+        del copy_only_int_keys[k]
+
+print(copy_only_int_keys)
