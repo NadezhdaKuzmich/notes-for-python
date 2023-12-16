acfb8a1ef21aa9fba009bf6979ce973d4226cce0
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2023-12-17 00:48:12 +0100

    add notes about functional

---
+++ functions/function.py
@@ -0,0 +1,113 @@
+# Функція – це об'єкт, який приймає та повертає значення. Функцію можна викликати з будь-якого місця в коді
+# повторного використання. Ця концепція допомагає уникнути повторюваних ділянок коду, а також розбивати програму
+# на підпрограми, що покращує читання коду.
+
+# Створення функції:
+# Щоб створити функцію, потрібно скористатися спеціальним словом def. Синтаксис:
+# def my_function(params?):
+#     тіло функції...
+
+# В тілі функції обов'язково щось має бути
+# def example_error():
+
+# example_error()  # => IndentationError: expected an indented block after function definition on line 14
+
+# Якщо ви ще не знаєте що буде в функції, то можна поставити заглушку, щоб не виводило помилку
+# v1
+# def example():
+#     pass  # заглушка
+
+# example()
+
+# v2
+# def example2():
+#     ...  # заглушка
+
+# example2()
+
+# v3
+# def example3():
+#     """Docstring for information"""  # заглушка (інформативний опис)
+
+# example3()
+# print(help(example3))  # => Docstring for information
+
+def print_sum():
+    print(2 + 2)
+
+
+print_sum()  # => 4
+
+
+def print_sum(a, b):
+    print(a + b)
+
+
+print_sum(4, 4)  # => 8
+print_sum(6, 8)  # => 14
+
+
+def printing_positional_arguments(first, second):
+    print(f'{first=} {second=}')
+
+
+printing_positional_arguments('FIRST', 'SECOND')  # => first='FIRST' second='SECOND'
+printing_positional_arguments('SECOND', 'FIRST')  # => first='SECOND' second='FIRST'
+
+
+# printing_positional_arguments('SECOND', 'FIRST', 'THIRD')  # => TypeError: printing_positional_arguments()
+# takes 2 positional arguments but 3 were given
+
+def printing_positional_arguments(first, second, third, *args):
+    print(f'{first=} {second=} {third=}', args)
+
+
+printing_positional_arguments('SECOND', 'FIRST', 'THIRD', 'fourth', 'fifth', '...', [], {}, 2)
+
+
+# => first='SECOND' second='FIRST' third='THIRD' ('fourth', 'fifth', '...', [], {}, 2)
+
+def print_sum(a, b, *nums):
+    print(a + b + sum(nums))
+
+
+print_sum(1, 2, 3, 4, 5, 6, 7, 8, 9)  # => 45
+
+
+def make_pizza(*toppings):
+    for topping in toppings:
+        print(f'-{topping}')
+
+
+make_pizza('mushrooms', 'sausages', 'cheese')
+
+
+# Ключові аргументи
+def printing_positional_arguments(first, second, third):
+    print(f'{first=} {second=} {third=}')
+
+
+printing_positional_arguments(second='SECOND', first='FIRST', third='THIRD')
+
+
+# => first='FIRST' second='SECOND' third='THIRD'
+
+def hello_user(name, surname, middlename):
+    print(f'Hello {name} {surname} {middlename}!')
+
+
+hello_user(surname='Dil', middlename='Mon', name='Mark')  # => Hello Mark Dil Mon!
+
+
+# hello_user(surname='Dil', name='Mark')  # => TypeError: hello_user() missing 1 required positional
+# argument: 'middlename'
+
+# *args - упаковка для позиційних аргументів
+# **kwargs - упаковка для ключових аргументів
+def hello_user(position_job, name, surname, middlename='', **kwargs):  # або None
+    print(kwargs)  # => {'country': 'Ukraine', 'work_car': 'CAT'}
+    print(f'Hello {name} {surname} {middlename}! Your job position is {position_job}.')
+
+
+hello_user('developer', surname='Dil', name='Mark', country="Ukraine", work_car='CAT')
+# => Hello Mark Dil ! Your job position is developer.
---
+++ functions/function_return.py
@@ -0,0 +1,113 @@
+# Для того, щоб повернути результат функції, потрібно скористатися спеціальним словом return.
+def sum_numbers(a, b):
+    return a + b
+
+
+result = sum_numbers(3, 9)
+print(result)  # => 12
+
+
+def subtraction_numbers(a, b):
+    answer = [a - b]
+    return answer
+
+
+result = subtraction_numbers(9, 3)
+print(result)  # => [6]
+
+
+def sum_numbers(a, b, *args):
+    answer = a + b + sum(args)
+    return answer
+
+
+result = sum_numbers(2, 3, 1, 3, 6, 2, 1, 8)
+print(result)  # => 26
+print(type(sum_numbers(2, 4)))  # => <class 'int'>
+
+
+def external():
+    def internal():
+        pass
+
+    return internal
+
+
+print(type(external()))  # => <class 'function'>
+
+
+def reversing_list_numbers(nums):
+    # nums_copy = nums.copy()
+    nums_copy = nums[:]
+    nums_copy.reverse()
+    return nums_copy
+
+
+numbers = [1, 2, 3]
+result = reversing_list_numbers(numbers)
+print(numbers)  # => [1, 2, 3]
+print(result)  # => [3, 2, 1]
+
+# Задачки:
+""" 1. Знайдіть максимальне значення в списку, що передається в функцію, і поверніть його, якщо воно більше 0, в іншому
+випадку поверніть повідомлення про те, що число менше 0. """
+# v1
+def get_max_from_list(list_values):
+    max_value = list_values[0]
+    for element in list_values:
+        if element > max_value:
+            max_value = element
+    return max_value if max_value > 0 else "Max value is less then zero"
+
+
+list_nums = [-45, -2, -5, -7, -78]
+max_a = get_max_from_list(list_nums)
+max_b = get_max_from_list([1, 12, 18, 5, 9, 102])
+print(max_a)  # => Max value is less then zero
+print(max_b)  # => 102
+
+
+# v2
+def get_max_from_list(list_values):
+    max_value = max(list_values)
+    return max_value if max_value > 0 else "Max value is less then zero"
+
+
+max_a = get_max_from_list([1, 4, 8, 0, 9])  # => 102
+max_b = get_max_from_list([-1, -2, -7, -9, -6])  # => Max value is less then zero
+print(max_a)
+print(max_b)
+
+""" 2. Поверніть кількість літер у слові, яке надсилається у параметрах. """
+def get_string_length(string):
+    return len(string)
+
+
+string_a = 'hello'
+string_b = 'bye'
+len_a = get_string_length(string_a)
+len_b = get_string_length(string_b)
+print(f"Str '{string_a}' has {len_a} characters")
+print(f"Str '{string_b}' has {len_b} characters")
+
+""" 3. Напишіть функцію, яка зводить до ступеня число, яке передається у параметрах, якщо ступінь
+не є негативним. Перший параметр - число, другий – ступінь, у якому його необхідно звести. """
+# v1
+def power(number, value_power):
+    if value_power >= 0:
+        return number ** value_power
+    return number
+
+
+print(power(2, -1))  # => 2
+
+
+# v2
+def power(number, value_power):
+    if value_power >= 0:
+        return pow(number, value_power)
+    return number
+    # print('NO PRINT')  # This code is unreachable
+
+
+print(power(3, 3))  # => 27
