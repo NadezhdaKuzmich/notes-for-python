74b1fb0c59308716b392b1bc22a4ecad3547de64
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-01-07 22:40:58 +0100

    add new theme

---
+++ python-advanced/multithreading/_threading/thread-lock.py
@@ -0,0 +1,32 @@
+# DEAD LOCK
+import threading
+
+
+def producer():
+    print('Set locking')
+    # беремо блокування
+    with lock:
+        print('done')
+        # Спроба взяти блокування в рамках поточного потоку дає нам DEAD LOCK
+        # З цього блокування не вийти, оскільки ми очікуємо завершення
+        # самого себе, щоб взяти блокування-блокування ніколи не
+        # звільниться.
+        # Актуально навіть у рамках одного потоку
+        with lock:
+            print("It's great")
+    print('Locking release!')
+
+
+# блокування, що дозволяє зазначити до якої ділянку коду атомарним.
+lock = threading.Lock()
+# __enter__ => lock.acquire()
+# __exit__ => lock.release()
+
+task1 = threading.Thread(target=producer)
+task2 = threading.Thread(target=producer)
+
+task1.start()
+task2.start()
+
+task1.join()
+task2.join()
