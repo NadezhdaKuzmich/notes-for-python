d2b9217d7a247d71f9857c7e51e4987725a65cdf
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-01-29 21:25:40 +0100

    add new examples

--- python-basic/set-tuples/set-methods.py
+++ python-basic/set-tuples/set-methods.py
@@ -1,19 +1,22 @@
 some_set = {'a', 1, 3, 4, 5, 2, 8}
 empty_set = set()
 
-# pop() – видаляє перший елемент множини і повертає його. Так як множини не впорядковані,
-# не можна точно сказати, який елемент буде першим.
+# pop() – видаляє перший елемент множини і повертає його. Так як множини не
+# впорядковані, не можна точно сказати, який елемент буде першим.
 print('pop:', some_set.pop())  # => 1
-print('pop:', some_set)  # => {2, 3, 4, 5, 8, 'a'} але відповідь може бути й інша
+print('pop:',
+      some_set)  # => {2, 3, 4, 5, 8, 'a'} але відповідь може бути й інша
 
 # print(empty_set.pop())  # => KeyError: 'pop from an empty set'
 
-# discard(elem) – видаляє елемент із множини, не повертає нічого (None) та не виводить помилок (None).
+# discard(elem) – видаляє елемент із множини, не повертає нічого (None) та
+# не виводить помилок (None).
 print('discard:', some_set.discard(8))  # => None
 print('discard:', some_set)  # => {2, 3, 4, 5, 'a'}
 # print(empty_set.discard('any'))  # => None
 
-# remove(elem) – видаляє елемент із множини. Якщо такого елемента немає, видає помилку.
+# remove(elem) – видаляє елемент із множини. Якщо такого елемента немає,
+# видає помилку.
 new_some_set = {'a', 1, 3, 4, 5, 2, 8}
 new_some_set.remove('a')  # => {1, 2, 3, 4, 5, 8}
 print('remove:', new_some_set)
@@ -27,10 +30,12 @@ print('add:', some_set)  # => {'new element', 2, 3, 4, 5, 'a'}
 # union(another_set) – поєднує множини.
 first_set = {'one', 'two'}
 second_set = {'three', 'four'}
-print('union:', first_set.union(second_set))  # => {'three', 'one', 'four', 'two'}
+print('union:',
+      first_set.union(second_set))  # => {'three', 'one', 'four', 'two'}
 
 # v2
-print(first_set | some_set)  # => {'new element', 2, 3, 4, 5, 'two', 'one', 'a'}
+print(
+    first_set | some_set)  # => {'new element', 2, 3, 4, 5, 'two', 'one', 'a'}
 
 # v3
 # Не повертає нову множину, а працює з першою
@@ -48,9 +53,14 @@ print(some_set1.intersection(some_set2, first_set))  # => set()
 print(some_set1 & some_set2)  # => {8, 'a'}
 print(some_set & some_set1 & some_set2)  # => {'a'}
 
-# difference(another_set) – різниця множин. Повертає різницю двох або більше множин у вигляді нової множини.
+# difference(another_set) – різниця множин. Повертає різницю двох або більше
+# множин у вигляді нової множини.
 print(some_set1.difference(some_set2))  # => {0, 2, 4, 6}
 print(some_set2.difference(some_set1))  # => {1, 3, 5, 7}
+a = {1, 2, 3, 4}
+b = {3, 4}
+print(a.difference(b))  # => {1, 2}
+print(b.difference(a))  # => set()
 
 # isdisjoint()
 # Перевірка того, що множина не має спільних елементів з іншою множиною
@@ -76,7 +86,8 @@ print({1, 2}.symmetric_difference({1, 2, 3}))  # => {3}
 print({1, 2} ^ {1, 2, 3})  # => {3}
 # АЛЕ! Бінарні оператори потребують щоб другий операнд також був множиною:
 # print({1, 2}.symmetric_difference([1, 2, 3]))  # => (Все спрацює) {3}
-# print({1, 2} ^ [1, 2, 3])  # => (Видасть помилку) TypeError: unsupported operand type(s) for ^: 'set' and 'list'
+# print({1, 2} ^ [1, 2, 3])  # => (Видасть помилку) TypeError: unsupported
+# operand type(s) for ^: 'set' and 'list'
 
 # clear() – очищає множину.
 some_set.clear()
--- python-basic/set-tuples/tuple-methods.py
+++ python-basic/set-tuples/tuple-methods.py
@@ -5,15 +5,17 @@ print('H' in some_tuple2)  # => True
 print(some_tuple2.count('l'), 'count')  # => 2 count
 print(some_tuple2.index('e'), 'index')  # => 1 index
 
-a, b, c = (1, 2, 3)  # => 1 2 3
-print(a, b, c)
+a, b, c = (1, 2, 3)
+print(a, b, c)  # => 1 2 3
 # a, b, c = 1, 2, 3
 # print(a, b, c)  # => 1 2 3
+a = (1, 2)
+x, y = a
+print(f'{x=} {y=}')  # => x=1 y=2
 
-# Кортежам властиві всі методи списків, які його не змінюють. Дуже часто зустрічається випадок використання
-# кортежів у тому, щоб поміняти елементи місцями. Для цього достатньо прописати:
-# a, b = b, a
-# swapping
+# Кортежам властиві всі методи списків, які його не змінюють. Дуже часто
+# зустрічається випадок використання кортежів у тому, щоб поміняти елементи
+# місцями. Для цього достатньо прописати: a, b = b, a swapping
 a = 1
 b = 2
 
@@ -28,17 +30,20 @@ b = 'Two'
 c = 'Three'
 a, b = b, a
 print(f'{a=} {b=}')  # => a='Two' b='One'
-# a, b = (b, a, c)  # => ValueError: too many values to unpack (expected 2) Не можна так робити!
+# a, b = (b, a, c)  # => ValueError: too many values to unpack (expected 2)
+# Не можна так робити!
 
-a, b, *anything = (b, a, c, 1,3, 4, 'hello')
-print(f'{a=} {b=} {anything=}')  # => a='One' b='Two' anything=['Three', 1, 3, 4, 'hello']
+a, b, *anything = (b, a, c, 1, 3, 4, 'hello')
+print(f'{a=} {b=} {anything=}')  # => a='One' b='Two' anything=['Three', 1,
+# 3, 4, 'hello']
 
 first, *middle, last = ('Igor', 'Mark', 'Lena', 'Zurab')
-print(f'{first=}, {middle=}, {last=}')  # => first='Igor', middle=['Mark', 'Lena'], last='Zurab'
+print(f'{first=}, {middle=}, {last=}')  # => first='Igor', middle=['Mark',
+# 'Lena'], last='Zurab'
 
-# Також, при проходженні за списком кортежів, цикл for може набувати стільки значень, скільки елементів у tuple.
-# Синтаксис:
-# for elem1, elem2 in [(1,2), (2,4)]:
+# Також, при проходженні за списком кортежів, цикл for може набувати стільки
+# значень, скільки елементів у tuple. Синтаксис: for elem1, elem2 in [(1,2),
+# (2,4)]:
 class_school = (('Igor', 10), ('Mark', 12), ('Lena', 8), ('Zurab', 11))
 for st, mark in class_school:
-    print(f'Student:{st}, mark:{mark}')
+    print(f'Student: {st}, mark: {mark}')
