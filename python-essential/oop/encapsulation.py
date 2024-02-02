93f1c0e0cf8fb736a5dfbf0d3b13c5ea27e81712
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-02-02 20:17:14 +0100

    add new examples

--- python-essential/oop/encapsulation.py
+++ python-essential/oop/encapsulation.py
@@ -1,18 +1,34 @@
-# ● Інкапсуляція - ще одне з основних понять, на якому будується ООП. Вона потрібна для того, щоб організувати рівні
-# доступу до різних архітектурних компонентів, які пише розробник.
+# ● Інкапсуляція - ще одне з основних понять, на якому будується ООП. Вона
+# потрібна для того, щоб організувати рівні доступу до різних архітектурних
+# компонентів, які пише розробник.
 
-# Є три можливі рівні доступу: private (приватний), protected (захищений) та public (публічний).
-# ● Інкапсуляція в Python відрізняється від її реалізації в інших мовах програмування і носить тут більш неявний
-# характер. Якщо в інших мовах використовуються ключові слова на кшталт public, protected і private, то Python
-# достатньо додати символ “_” (один символ нижнього підкреслення) перед іменем змінної або функції, щоб зробити
-# її protected, а “__ ” (два символи нижнього підкреслення) щоб зробити private. Public є всі інші імена, без
-# символів “_” перед ними.
-# ● У даного підходу є свої переваги та недоліки. Перевагою, безумовно, є простота реалізації. Вам не потрібно
-# використовувати ключові слова, а достатньо додати “_” або “__” перед ім'ям змінної або функції або взагалі не
-# додавати. Недоліком є недосконалість даного підходу. Приватні змінні за фактом не є такими. Якщо в інших мовах
-# програмування ви ніяк не зможете до них "доступитися" поза класом, то в Python, якщо у вас є клас A, у нього є
-# атрибут self._x= 10, то після того, як ви створите об'єкт класу a = A(), то якщо ви зробите print(a.x) буде
-# помилка, але якщо ви зробите print(a._A_x), то виведеться значення змінної х = 10
+# Є три можливі рівні доступу:
+# 1. private (приватний)
+# 2. protected (захищений)
+# 3. public (публічний)
+
+# Інкапсуляція - принцип, у якому користувач використовує лише публічну чи
+# “інтерфейсну” частину класу і не вникає у його внутрішню реалізацію. Таким
+# чином, програміст приховує певні дані, методи, атрибути від наступних
+# користувачів.
+
+# ● Інкапсуляція в Python відрізняється від її реалізації в інших мовах
+# програмування і носить тут більш неявний характер. Якщо в інших мовах
+# використовуються ключові слова на кшталт public, protected і private,
+# то Python достатньо додати символ “_” (один символ нижнього підкреслення)
+# перед іменем змінної або функції, щоб зробити її protected, а “__ ” (два
+# символи нижнього підкреслення) щоб зробити private. Public є всі інші
+# імена, без символів “_” перед ними.
+
+# ● У даного підходу є свої переваги та недоліки. Перевагою, безумовно,
+# є простота реалізації. Вам не потрібно використовувати ключові слова,
+# а достатньо додати “_” або “__” перед ім'ям змінної або функції або
+# взагалі не додавати. Недоліком є недосконалість даного підходу. Приватні
+# змінні за фактом не є такими. Якщо в інших мовах програмування ви ніяк не
+# зможете до них "доступитися" поза класом, то в Python, якщо у вас є клас
+# A, у нього є атрибут self._x= 10, то після того, як ви створите об'єкт
+# класу a = A(), то якщо ви зробите print(a.x) буде помилка, але якщо ви
+# зробите print(a._A_x), то виведеться значення змінної х = 10
 
 class BankAccount:
     def __init__(self, name, balance, passport):
@@ -34,11 +50,13 @@ account = BankAccount('Vadim', 10000, 'EK82S49R')
 account.print_private_data()  # => Vadim 10000 EK82S49R
 
 
-# print(account._name)  # => Якщо поле захищене до нього все одно можна отримати доступ за межами класу
+# print(account._name)  # => Якщо поле захищене до нього все одно можна
+# отримати доступ за межами класу
 # print(account._balance)
 # print(account._passport)
 
-# print(account.__name)  # => AttributeError: 'BankAccount' object has no attribute '__name'
+# print(account.__name)  # => AttributeError: 'BankAccount' object has no
+# attribute '__name'
 # print(account.__balance)
 # print(account.__passport)
 
@@ -46,10 +64,12 @@ account.print_private_data()  # => Vadim 10000 EK82S49R
 # print(account._BankAccount__balance)  # => 10000
 
 
-# Законним способом працювати з приватними та захищеними змінними є використання таких функцій як getters та setters.
-# Це спеціальні функції самого класу, які ви самі прописуєте та вирішуєте, як саме вони повинні повертати значення
-# приватних/захищених змінних (getters, наприклад за паролем або просто так) та як встановлювати значення таких
-# атрибутів (setters).
+# Законним способом працювати з приватними та захищеними змінними є
+# використання таких функцій як getters та setters. Це спеціальні функції
+# самого класу, які ви самі прописуєте та вирішуєте, як саме вони повинні
+# повертати значення приватних/захищених змінних (getters, наприклад за
+# паролем або просто так) та як встановлювати значення таких атрибутів (
+# setters).
 class BankAccount2:
     def __init__(self, name, balance):
         self.name = name
@@ -76,6 +96,8 @@ account2.name = "Nadijka"
 print(account2.balance)
 account2.balance = 5999
 print(account2.balance)
+
+
 # account2.balance = 'Zero'
 # account2.set_balance('Zero')  # => ValueError: Only number!
 
@@ -137,3 +159,52 @@ print(w1.salary)
 
 w2 = Worker('D')
 print(w2.salary)
+
+
+class TextProcessor:
+    def __init__(self):
+        self._punctuation = '.,!?;:*'
+
+    def __is_punctuation(self, char):
+        return char in self._punctuation
+
+    def get_clean_string(self, text):
+        clean_text = ""
+        for char in text:
+            if self.__is_punctuation(char):
+                continue
+            clean_text += char
+        return clean_text
+
+
+class TextLoader:
+    def __init__(self):
+        self.__text_processor = TextProcessor()
+        self.__clean_string = None
+
+    def set_clean_string(self, text):
+        self.__clean_string = self.__text_processor.get_clean_string(text)
+
+    @property
+    def clean_string(self):
+        print("Clean string is: {}".format(self.__clean_string))
+        return self.__clean_string
+
+
+class DataInterface:
+    def __init__(self):
+        self.__text_loader = TextLoader()
+
+    def process_texts(self, texts):
+        clean_texts = []
+        for text in texts:
+            self.__text_loader.set_clean_string(text)
+            clean_string = self.__text_loader.clean_string
+            clean_texts.append(clean_string)
+        return clean_texts
+
+
+data_i = DataInterface()
+data_texts = ['Hello, I am Nadiia *', 'Hey, what is the weather today ?']
+clean_t = data_i.process_texts(data_texts)
+print(clean_t)
