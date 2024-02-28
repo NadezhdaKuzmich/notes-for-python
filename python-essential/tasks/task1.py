ad4be5344d0a668ee4cf13c125d016d7df7f7053
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-02-28 20:01:55 +0100

    add new task

---
+++ python-essential/tasks/task1.py
@@ -0,0 +1,61 @@
+import copy
+
+# Завдання 1
+# Створіть клас, який описує книгу. Він повинен містити інформацію про автора,
+# назву, рік видання та жанр. Створіть кілька книг. Визначте
+# йому операції перевірки на рівність і нерівність, методи __repr__ і __str__.
+class Book(object):
+    def __init__(self, author, title, publication_year, genre):
+        self.author = author
+        self.title = title
+        self.publication_year = publication_year
+        self.genre = genre
+
+    def __eq__(self, other):
+        if not isinstance(other, Book):
+            return False
+        return self.author == other.author and \
+            self.title == other.title and \
+            self.publication_year == other.publication_year and \
+            self.genre == other.genre
+
+    def __as_string(self, format_string):
+        return format_string.format(
+            self.author,
+            self.title,
+            self.publication_year,
+            self.genre
+        )
+
+    def __str__(self):
+        return self.__as_string('"{}" by {} (published in {}, genre: {})')
+
+    def __repr__(self):
+        return self.__as_string("Book({!r}, {!r}, {!r}, {!r})")
+
+
+def main():
+    orwell1984 = Book('George Orwell', '1984', 1949,
+                      'dystopia')
+    orwell_copy = copy.copy(orwell1984)
+    learning_python = Book('Mark Lutz', 'Learning Python',
+                           2013, 'tutorial')
+
+    print(orwell1984)
+    print(learning_python)
+    print()
+
+    print(repr(orwell1984))
+    print(repr(learning_python))
+    print()
+
+    print(orwell1984 == orwell_copy)
+    print(orwell1984 != orwell_copy)
+    print(orwell1984 == learning_python)
+    print(orwell1984 != learning_python)
+    print(learning_python == learning_python)
+    print(learning_python != learning_python)
+
+
+if __name__ == '__main__':
+    main()
