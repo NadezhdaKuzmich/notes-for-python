50f1c74eb886fd9d2ca3bf02dd1e0ba38f63c01c
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-03-10 22:18:15 +0100

    edit file

--- python-essential/tasks/task22.py
+++ python-essential/tasks/task22.py
@@ -1,3 +1,4 @@
+# v1
 word = input('Enter word ')
 print("Original String:", word)
 size = len(word)
@@ -5,3 +6,11 @@ size = len(word)
 print("Print only even index chars")
 for i in range(0, size, 2):
     print("index[", i, "]", word[i])
+
+# v2
+word = input('Enter word ')
+print("Original String:", word)
+
+chars = list(word)
+for char in chars[::2]:
+    print(char)
