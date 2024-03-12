d94222a9e5390712de0582cb9d049bae22c75a27
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-03-12 20:19:08 +0100

    edit file'

---
+++ python-essential/tasks/task28.py
@@ -0,0 +1,5 @@
+string = ("Python is a popular programming language. Python can be used on a "
+          "server to create web applications. Learn Python.")
+
+count_of_word = string.count("Python")
+print(count_of_word)
