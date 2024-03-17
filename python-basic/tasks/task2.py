71a8b8cdcb0c2cdeeb7e156631bd681f28135a6d
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-03-17 22:24:49 +0100

    add new tasks

--- python-basic/tasks/task2.py
+++ python-basic/tasks/task2.py
@@ -15,3 +15,41 @@ for i in range(rows, 0, -1):
     for j in range(0, i - 1):
         print("*", end=' ')
     print("\r")
+
+# task 3
+str1 = "PYthON"
+print('Original String:', str1)
+lower = []
+upper = []
+
+for char in str1:
+    if char.islower():
+        lower.append(char)
+    else:
+        upper.append(char)
+
+sorted_str = ''.join(lower + upper)
+print('Result:', sorted_str)
+
+
+# task 4
+def find_digits_chars_symbols(sample_str):
+    char_count = 0
+    digit_count = 0
+    symbol_count = 0
+
+    for char in sample_str:
+        if char.isalpha():
+            char_count += 1
+        elif char.isdigit():
+            digit_count += 1
+        else:
+            symbol_count += 1
+
+    print(f"chars: {char_count}; digits: {digit_count}; "
+          f"symbols: {symbol_count}.")
+
+
+string = "P@yt2h&#o5n"
+print("Total counts of chars, digits, and symbols:")
+find_digits_chars_symbols(string)
