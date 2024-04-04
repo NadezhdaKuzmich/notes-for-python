88e6552e896f46e8a5c814271bd8c7539f4d872c
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-04 21:14:43 +0200

    add new tasks

--- python-basic/tasks/task16.py
+++ python-basic/tasks/task16.py
@@ -1,6 +1,8 @@
 import sys
 import datetime
 from math import pi
+import calendar
+from datetime import date
 
 # task 1
 print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp "
@@ -39,15 +41,15 @@ print("The extension of the file is : " + repr(f_extns[-1]))
 
 # task 7
 color_list = ["Red", "Green", "White", "Black"]
-print("%s %s" % (color_list[0], color_list[-1]))
+print("\n%s %s" % (color_list[0], color_list[-1]))
 
 # task 8
 # v1
-a = input("\nInput an integer: ")
-n1 = int("%s" % a)
-n2 = int("%s%s" % (a, a))
-n3 = int("%s%s%s" % (a, a, a))
-print(n1 + n2 + n3)
+# a = input("\nInput an integer: ")
+# n1 = int("%s" % a)
+# n2 = int("%s%s" % (a, a))
+# n3 = int("%s%s%s" % (a, a, a))
+# print(n1 + n2 + n3)
 
 # v2
 a = input("\nInput an integer: ")
@@ -55,3 +57,21 @@ n1 = int(a)
 n2 = int(a * 2)
 n3 = int(a * 3)
 print(n1 + n2 + n3)
+
+# task 9
+# A docstring is a string literal that occurs as the first statement in a
+# module, function, class, or method definition. Such a docstring becomes the
+# __doc__ special attribute of that object.
+print(f'\n{abs.__doc__}')
+
+# task 10
+y = int(input("Input the year : "))
+m = int(input("Input the month : "))
+print(calendar.month(y, m))
+
+# task 11
+f_date = date(2024, 3, 2)
+l_date = date(2024, 4, 1)
+delta = l_date - f_date
+# days - повертає кількість днів
+print(delta.days)
