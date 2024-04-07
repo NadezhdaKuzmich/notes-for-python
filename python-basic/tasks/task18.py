deb1dcb0296112ff0743af064e4fb4d56302147c
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-07 21:24:38 +0200

    add new tasks

--- python-basic/tasks/task18.py
+++ python-basic/tasks/task18.py
@@ -2,6 +2,10 @@ import struct
 # 'struct' module provides pack and unpack functions for working with
 # variable-length binary data.
 import platform
+import os
+import sys
+import sysconfig
+import site
 
 # task 1
 # v1
@@ -16,4 +20,25 @@ print(struct.calcsize("P") * 8)
 # v2
 architecture = platform.architecture()[0]
 print(architecture)
-print(struct.calcsize("P") * 8)
+
+# task 2
+# v1
+print("\nName of the operating system:   ", os.name)
+# 'Darwin' (macOS)
+print("Name of the OS system:          ", platform.system())
+print("Version of the operating system:", platform.release())
+# v2
+print("\nos.name                     ", os.name)
+print("sys.platform                ", sys.platform)
+print("platform.system()           ", platform.system())
+print("sysconfig.get_platform()    ", sysconfig.get_platform())
+print("platform.machine()          ", platform.machine())
+print("platform.architecture()[0]  ", platform.architecture()[0], '\n')
+
+# task 3
+# 'site.getsitepackages()' retrieves site packages' paths.
+print(site.getsitepackages())
+
+# task 4
+# 'os.path.realpath(__file__)' gets the full path of the current Python script.
+print("\nCurrent File Name: ", os.path.realpath(__file__))
