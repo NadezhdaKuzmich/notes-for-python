04dddfb3e867b0d6b52149ae6d0a16fb8d415cfc
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-11 21:06:02 +0200

    edit tasks

--- python-basic/tasks/task20.py
+++ python-basic/tasks/task20.py
@@ -1,4 +1,8 @@
 import os
+import time
+import socket
+import platform
+from functools import reduce
 
 # task 1
 # v1
@@ -13,7 +17,7 @@ file.seek(0, os.SEEK_END)  # move file cursor to the end of the file.
 print("The size of main.py is:", file.tell(), "bytes")
 
 # task 2
-x = b'Abc'  # b - bytes object
+x = b"Abc"  # b - bytes object
 print("\nConvert bytes of the said string to a list of integers:")
 print(list(x))
 
@@ -31,6 +35,26 @@ text = "a123"
 
 try:
     i = float(text)
-    print("The input value is numbers.")
+    print("The input value is numbers.\n")
 except (ValueError, TypeError):
-    print("Not numeric.")
+    print("Not numeric.\n")
+
+# task 4
+print(time.ctime(), "\n")
+
+# task 5
+# v1
+host_name = socket.gethostname()
+print("Host name:", host_name)
+# v2
+host_name = platform.uname()[1]
+print("Host name:", host_name)
+# v3
+host_name = os.uname().nodename
+print("Host name:", host_name, "\n")
+
+# task 6
+nums = [10, 20, 30]
+nums_product = reduce((lambda a, b: a * b), nums)
+print("\nProduct of the said numbers (without using a for loop):",
+      nums_product)
